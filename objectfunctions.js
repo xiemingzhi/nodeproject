d9d0f813152de9057640b98f140ede895a782ba4
xiemingzhi <xiemingzhi@gmail.com>
2018-10-14 14:05:49 +0800

    Private member with public method using ES5

--- objectfunctions.js
+++ objectfunctions.js
@@ -13,11 +13,21 @@ person.talk();
 
 //Constructor function
 function Person(name) {
+    let privateMember = 'private string';
     this.name = name;
     this.talk = function () {
         console.log('a person can talk2');
     }
+    //ECMAScript 5 is also known as ES5 and ECMAScript 2009
+    //Object property methods
+    Object.defineProperty(this, 'showString', {
+        get: function() {
+            return privateMember;
+        }
+    });
 }
 
 const person2 = new Person('ted2');
 person2.talk();
+console.log('show string ' + person2.privateMember);//show string undefined
+console.log('show string ' + person2.showString);
