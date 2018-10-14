e28cd6b83267641bac44d8c8f104e8faf9028725
xiemingzhi <xiemingzhi@gmail.com>
2018-10-14 21:14:10 +0800

    Prototype example
    Use composition inherit only what you need
    ES5 Object.create()
    ES6 class, constructor, super, extends

--- objectfunctions.js
+++ objectfunctions.js
@@ -31,3 +31,36 @@ const person2 = new Person('ted2');
 person2.talk();
 console.log('show string ' + person2.privateMember);//show string undefined
 console.log('show string ' + person2.showString);
+
+//Using prototypes for object inheritance
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+let f = function () {
+    this.a = 1;
+    this.b = 2;
+}
+let o = new f();
+f.prototype.b = 3;
+f.prototype.c = 4;
+console.log(o.a);
+console.log(o.b);
+console.log(o.c);
+console.log(o.d);
+
+function Teacher(name) {
+    this.name = name;
+    this.lecture = function() {
+        console.log(this.name + ' i can teach');
+    }
+}
+
+function talkFast() {
+    console.log('I am a teacher I talk fast.');
+}
+//use composition inherit only what you need
+//Teacher.prototype.talk = Person.talk; does not work
+Teacher.prototype.talk = person2.talk;
+//Teacher.prototype.talk = talkFast;//overriding
+const goodTeacher = new Teacher('Mr Smith');
+//goodTeacher.prototype.talk = person2.talk; does not work
+goodTeacher.talk();//no prototype - goodTeacher.talk is not a function
+goodTeacher.lecture();
