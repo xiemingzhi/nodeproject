3846a86d89469b30c0a51382bd11e746b086731c
ming <xiemingzhi@gmail.com>
2020-03-15 10:03:35 +0800

    Add promises example

--- promises-example.js
+++ promises-example.js
@@ -1,2 +1,24 @@
 //es6 ecmascript 2015 
-callAPIAsync(audioSettings).then(successCallback, failureCallback);
+let callAPIAsync = new Promise((resolve, reject) => {
+    // make API call
+    //if success call resolve 
+    setTimeout( function() {
+      resolve("Success!")  // Yay! Everything went well!
+    }, 250) 
+    //else call reject
+  }) 
+
+let successCallback = (successMessage) => {
+    console.log("Yay! " + successMessage) 
+  }
+
+let failureCallback = (failureMessage) => {
+    console.log("Error " + failureMessage) 
+  }
+
+callAPIAsync.then(successCallback, failureCallback);
+
+console.log("Finish Setup")
+//promises can be chained 
+//also use catch to handle errors/default case
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
