3a66a4c6d46302b23906731281b1cf83126c4a5f
ming <xiemingzhi@gmail.com>
2020-03-17 18:40:59 +0800

    Add closure example

---
+++ closure-example.js
@@ -0,0 +1,60 @@
+function makeFunc() {
+  var name = "Mozilla";
+  function displayName() {
+    console.log("makefunc", name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc();
+
+//=======================================================================
+//function factory - function to create other functions
+function makeAdder(x) {
+  console.log("makeadder", x);
+  return function(y) {
+    console.log("inner function", y);
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
+//=======================================================================
+//encapsulation
+var makeCounter = function() {
+  var privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+
+    decrement: function() {
+      changeBy(-1);
+    },
+
+    value: function() {
+      return privateCounter;
+    }
+  };
+};
+
+var counter1 = makeCounter();
+var counter2 = makeCounter();
+
+console.log(counter1.privateCounter) //undefined
+console.log('counter1',counter1.value()); // 0.
+console.log('counter2',counter2.value()); // 0.
+
+counter1.increment();
+console.log('counter1',counter1.value()); // 1.
+console.log('counter2',counter2.value()); // 0.
+
