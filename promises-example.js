4570056f6fe59c286d924993faa4de82fb6d7f45
ming <xiemingzhi@gmail.com>
2020-03-18 09:33:05 +0800

    Promises error handling

--- promises-example.js
+++ promises-example.js
@@ -25,3 +25,16 @@ console.log("Finish Setup")
 //promises can be chained 
 //also use catch to handle errors/default case
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+//https://developers.google.com/web/fundamentals/primers/promises#error_handling
+//error handling
+//With then(func1, func2), func1 or func2 will be called, never both. 
+//i.e if error occurs in successCallback, failureCallback will not be called
+//Promise rejections skip forward to the next then() with a rejection callback (or catch(), since it's equivalent). 
+//with then(successCallback).catch(failureCallback) successCallback will be executed and if error, skip next then(), and will call failureCallback
+
+let problemSuccessCallback = (successMessage) => {
+  throw Error('problem in callback') 
+}
+callAPIAsync.then(problemSuccessCallback).catch(failureCallback);
+console.log("Finish Setup2")
