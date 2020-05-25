b743f2c0201793bd376d9aa18d9469f3f5ea48aa
ming <xiemingzhi@gmail.com>
2020-05-25 12:38:07 +0800

    Add curry example
    reduce functions of more than one argument to functions of one argument
    Currying is a transformation of functions that translates a function from callable as
    f(a, b, c) into callable as f(a)(b)(c).

---
+++ curry-example.js
@@ -0,0 +1,16 @@
+function curry(f) { // curry(f) does the currying transform
+    return function(a) {
+      return function(b) {
+        return f(a, b);
+      };
+    };
+  }
+  
+  // usage
+  function sum(a, b) {
+    return a + b;
+  }
+  
+  let mySum = curry(sum);
+  
+  console.log( mySum(1)(2) ); // 3
